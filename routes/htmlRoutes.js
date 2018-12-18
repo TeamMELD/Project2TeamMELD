3147bd54010d136baf221dd9fcc90f6c8bd7f537
Elizabeth Adamczyk <emadamczyk@hotmail.com>
2018-12-17 21:44:09 -0600

    update to front end index

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Report.findAll({}).then(function(data) {
+    db.Report.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
