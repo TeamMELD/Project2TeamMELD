e85479fa9dc9a33e11cd603577f52325af7013da
Luis Cartagena <luiscartagena2017@u.northwestern.edu>
2018-12-17 21:38:12 -0600

    update htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,29 +3,36 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Report.findAll({}).then(function(data) {
+    db.Report.findAll({}).then(function(reportdb) {
       res.render("index", {
-        msg: "Welcome!",
-        examples: data
+        msg: "Welcomeeeee!",
+        report: reportdb
       });
     });
   });
   
   //Load report page
 
-  // Load search page
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  // Load example page and pass in an example by id
+  app.get("/report/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(reportdb) {
+      res.render("report", {
+        example: reportdb
       });
     });
   });
 
-  //Load search page for zipcode
 
-  //load search page for articles related to issue
-  
+// Load example page and pass in an example by id
+// app.get("/example/:id", function(req, res) {
+//   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//     res.render("example", {
+//       example: dbExample
+//     });
+//   });
+// });
+
+
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
