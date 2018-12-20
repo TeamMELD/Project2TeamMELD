7e32ffc3d96b6ae1523ed1d8f9a89ccb5d82120d
Duncan Dam <duncandam@dhcp-10-105-239-28.wireless.northwestern.private>
2018-12-19 20:43:31 -0600

    html routes updated

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,40 +1,27 @@
-var db = require("../models");
+// var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Report.findAll({}).then(function(reportdb) {
-      res.render("index", {
-        msg: "Welcomeeeee!",
-        report: reportdb
-      });
-    });
-  });
-  
-  //Load report page
-
-  // Load example page and pass in an example by id
-  app.get("/report/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(reportdb) {
-      res.render("report", {
-        example: reportdb
-      });
-    });
+    res.render("index");
   });
 
+  //load report page
+  app.get("/report", function(req, res) {
+    res.render("./partials/report");
+  });
 
-// Load example page and pass in an example by id
-// app.get("/example/:id", function(req, res) {
-//   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-//     res.render("example", {
-//       example: dbExample
-//     });
-//   });
-// });
+  //load search page
+  app.get("/search", function(req, res) {
+    res.render("search");
+  });
 
+  //load other page
+  app.get("/others", function(req, res) {
+    res.render("others");
+  });
 
-
-  // Render 404 page for any unmatched routes
+  //load error page
   app.get("*", function(req, res) {
     res.render("404");
   });
