dfcef2ebe463cd132e148f145ecccfc0e89677a8
Elizabeth Adamczyk <emadamczyk@hotmail.com>
2019-01-06 11:40:33 -0600

    fixed modal for successfully submitted report, working a validation feedback to user

--- public/js/reports.js
+++ public/js/reports.js
@@ -3,8 +3,34 @@ $(document).ready(function () {
     //submit form with submit on click button 
     $("form.report-form").on("submit", function (event) {
         event.preventDefault();
-
+        console.log("In submit")
         //create variables to store value from input
+        // check the type of input
+        // check the exist 
+        // HTML5 validation ... EA is working on this today (1/6)
+        // let username = $("#username").val().trim();
+        // if(!username.length > 3 || typeof username != 'string' ){
+            
+        //     $("#submissionModal").modal('show');
+        //     $('.modal-title').text('Submission Error')
+        //     $('.modal-body').text('Invalid Name; needs more than 3 characters.')
+        // }
+        // username.addEventListener("input", function (event) {
+        //     if (username.validity.typeMismatch) {
+        //       username.setCustomValidity("Please enter a full name of more than 3 characters");
+        //     } else {
+        //       username.setCustomValidity("");
+        //     }
+        //   });
+        //   let zipcode = $("#zipcode").val().trim();
+        //   zipcode.addEventListener("input", function (event) {
+        //     if (zipcode.validity.typeMismatch) {
+        //       zipcode.setCustomValidity("Please enter a 5 digit zip code");
+        //     } else {
+        //       zipcode.setCustomValidity("");
+        //     }
+        //   });
+
         let newReport = {
             username: $("#username").val().trim(),
             address: $("#address").val().trim(),
@@ -15,7 +41,8 @@ $(document).ready(function () {
             categories: $("#category").val().trim(),
             rating: $("#rating").val().trim()
         };
-        // console.log(newReport);
+       
+         console.log(newReport);
 
         //Ajax call to post report back into the database
         $.ajax("/api/reports", {
@@ -23,15 +50,24 @@ $(document).ready(function () {
             data: newReport
         }).then(function () {
             console.log("New report is on database woohoo!");
-            location.reload();
-
+            $("#submissionModal").modal('show');
+            $('.modal-title').text('Your submission was successful')
+            $('.modal-body').text('Thank you for your report.')
+            
+            $("#username").val("")
+            $("#address").val("")
+            $("#city").val("")
+            $("#state").val("")
+            $("#zipcode").val("")
+            $("#violationDescription").val("")
+            $("#category").val("")
+            $("#rating").val("")
+            
         });
 
     })
     //open modal after submission
-    $(window).load(function () {
-        $("#submissionModal").modal('show');
-    });
+  
 
 });
 
