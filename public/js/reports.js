d67aeae7d0587996c0b5bc252bf1b057d3a6c1f3
Duncan Dam <duncandam@dhcp-10-101-9-153.wireless.northwestern.private>
2019-01-09 16:55:52 -0600

    validation JQuery update -- almost die

--- public/js/reports.js
+++ public/js/reports.js
@@ -2,64 +2,57 @@ $(document).ready(function () {
     //console.log("ok it's running");
     //submit form with submit on click button 
     $("#reportForm").validate({
-    //$("form").on("submit", function (event) {
+        //$("form").on("submit", function (event) {
         // event.preventDefault();
         // console.log("In submit")
-        submitHandler: function(form) {
-            $(form).ajaxSubmit();
-
-        
-        //jQuery validation - this script not needed
-        
-            // rules: {
-            //     state: {
-            //         required: true
-            //     },
-            //     category: {
-            //         required: true
-            //     }
-            // }
-       
-
-        let newReport = {
-            username: $("#username").val().trim(),
-            address: $("#address").val().trim(),
-            city: $("#city").val().trim(),
-            state: $("#state").val().trim(),
-            zipcode: $("#zipcode").val().trim(),
-            violation_description: $("#violationDescription").val().trim(),
-            categories: $("#category").val().trim(),
-            rating: parseFloat($("#rating").val().trim())
-        }      
-
-        console.log(newReport);
-        
-        //Ajax call to post report back into the database
-        $.ajax("/api/reports", {
-            type: "POST",
-            data: newReport
-        }).then(function () {
-            console.log("New report is on database woohoo!");
-            //open modal after submission
+        submitHandler: function (form) {
+             //open modal after submission
             $("#submissionModal").modal('show');
             $('.modal-title').text('Your submission was successful')
             $('.modal-body').text('Thank you for your report.')
+            $("#reportForm").click(function(){
+               form.submit(); 
+            })
             
-            $("#username").val("")
-            $("#address").val("")
-            $("#city").val("")
-            $("#state").val("")
-            $("#zipcode").val("")
-            $("#violationDescription").val("")
-            $("#category").val("")
-            $("#rating").val("")
-            
-        })
-    }
+            let newReport = {
+                username: $("#username").val().trim(),
+                address: $("#address").val().trim(),
+                city: $("#city").val().trim(),
+                state: $("#state").val().trim(),
+                zipcode: $("#zipcode").val().trim(),
+                violation_description: $("#violationDescription").val().trim(),
+                categories: $("#category").val().trim(),
+                rating: parseFloat($("#rating").val().trim())
+            }
+
+            console.log(newReport);
+
+            //Ajax call to post report back into the database
+            $.ajax("/api/reports", {
+                type: "POST",
+                data: newReport
+            }).then(function () {
+                console.log("New report is on database woohoo!");
+                //open modal after submission
+                // $("#submissionModal").modal('show');
+                // $('.modal-title').text('Your submission was successful')
+                // $('.modal-body').text('Thank you for your report.')
+
+                $("#username").val("")
+                $("#address").val("")
+                $("#city").val("")
+                $("#state").val("")
+                $("#zipcode").val("")
+                $("#violationDescription").val("")
+                $("#category").val("")
+                $("#rating").val("")
+
+            })
+        }
 
     })
-    
-  
+
+
 
 });
 
