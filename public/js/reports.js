31ab1e2d2d50cb560e055e351d1340203e2b59b0
Duncan Dam <duncandam@dhcp-10-105-4-49.wireless.northwestern.private>
2019-01-09 21:15:46 -0600

    fixed report.js

--- public/js/reports.js
+++ public/js/reports.js
@@ -2,36 +2,13 @@ $(document).ready(function () {
     //console.log("ok it's running");
     //submit form with validation and modal confirmation 
     $("#reportForm").validate({
-        submitHandler: function(form) {
+        submitHandler: function (form) {
             $("#submissionModal").modal('show');
-            $('.modal-title').text('Your submission was successful')
-            $('.modal-body').text('Thank you for your report.')
-            $("#reportForm").click(function(){
+            $('.modal-title').text('Your submission was successful');
+            $('.modal-body').text('Thank you for your report.');
+            $("#reportForm").click(function () {
                 form.submit()
-            })    
-       
-
-        let newReport = {
-            username: $("#username").val().trim(),
-            address: $("#address").val().trim(),
-            city: $("#city").val().trim(),
-            state: $("#state").val().trim(),
-            zipcode: $("#zipcode").val().trim(),
-            violation_description: $("#violationDescription").val().trim(),
-            categories: $("#category").val().trim(),
-            rating: parseFloat($("#rating").val().trim())
-        }      
-
-        console.log(newReport);
-        
-        //Ajax call to post report back into the database
-        $.ajax("/api/reports", {
-            type: "POST",
-            data: newReport
-        }).then(function () {
-            console.log("New report is on database woohoo!");
-                   
-            
+            })
             let newReport = {
                 username: $("#username").val().trim(),
                 address: $("#address").val().trim(),
@@ -51,23 +28,9 @@ $(document).ready(function () {
                 data: newReport
             }).then(function () {
                 console.log("New report is on database woohoo!");
-                
-                $("#username").val("")
-                $("#address").val("")
-                $("#city").val("")
-                $("#state").val("")
-                $("#zipcode").val("")
-                $("#violationDescription").val("")
-                $("#category").val("")
-                $("#rating").val("")
 
             })
         }
-
     })
-
-
-
-});
-
+})
 
